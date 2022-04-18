7ad6f1c37486643fe1b01a60876b906d38720ace
Pavel Lavrenkov <beasty2006@yandex.ru>
2022-04-18 22:38:57 +0300

    PW10 hotfix after reviw v1

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+import PopupWithForm from './PopupWithForm';
+import ImagePopup from './ImagePopup';
 
 function App() {
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
@@ -35,7 +37,6 @@ function App() {
     setIsEditAvatarPopupOpen(false);
     setIsEditProfilePopupOpen(false);
     setSelectedCard(null);
-    document.querySelector('.pop-up_opened').classList.remove('pop-up_opened');
   }
 
   return (
@@ -44,16 +45,35 @@ function App() {
         <Header />
         <Main
           onEditProfile={handleEditProfileClick}
-          isOpenProfile={isEditProfilePopupOpen}
           onAddPlace={handleAddPlaceClick}
-          isOpenPlace={isAddPlacePopupOpen}
           onEditAvatar={handleEditAvatarClick}
-          isOpenAvatar={isEditAvatarPopupOpen}
           closeAllPopups={closeAllPopups}
           onCardClick={handleCardClick}
-          card={selectedCard}
         />
         <Footer />
+        <PopupWithForm name={"profile"} title={"Редактировать профиль"} button={"Сохранить"} isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+          <input type="text" className="pop-up-form__field" placeholder="Введите другое имя" id="pop-up-form-profile-title"
+            name="title" required minLength="2" maxLength="40" autoComplete="name" />
+          <span className="pop-up-form__input-error pop-up-form-profile-title-error"></span>
+          <input type="text" className="pop-up-form__field" placeholder="Введите занятие" id="pop-up-form-profile-added-info"
+            name="info" required minLength="2" maxLength="200" autoComplete="off" />
+          <span className="pop-up-form__input-error pop-up-form-profile-added-info-error"></span>
+        </PopupWithForm>
+        <PopupWithForm name={"avatar-edit"} title={"Обновить аватар"} button={"Сохранить"} isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
+          <input type="url" className="pop-up-form__field" placeholder="Ссылка на картинку" id="pop-up-form-avatar-url"
+            name="info" required autoComplete="url" />
+          <span className="pop-up-form__input-error pop-up-form-avatar-url-error"></span>
+        </PopupWithForm>
+        <PopupWithForm name={"newcard"} title={"Новое место"} button={"Сохранить"} isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+          <input type="text" className="pop-up-form__field" placeholder="Название" id="pop-up-form-newcard-title"
+            name="title" required minLength="2" maxLength="30" autoComplete="off" />
+          <span className="pop-up-form__input-error pop-up-form-newcard-title-error"></span>
+          <input type="url" className="pop-up-form__field" placeholder="Ссылка на картинку"
+            id="pop-up-form-newcard-added-info" name="info" required autoComplete="url" />
+          <span className="pop-up-form__input-error pop-up-form-newcard-added-info-error"></span>
+        </PopupWithForm>
+        <PopupWithForm name={"deletecard"} title={"Вы уверены?"} button={"Да"} onClose={closeAllPopups} />
+        <ImagePopup onClose={closeAllPopups} cardAttributes={selectedCard} />
       </div>
     </div>
   );
